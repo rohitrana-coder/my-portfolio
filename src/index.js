cc9a35b0e2568e9b641f51241da1fe76875b90b8
rohitrana-coder <rohitrana9992@gmail.com>
2025-09-29 11:47:48 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,7 +15,8 @@ import Work from "./Work";
   
 export default function App() {
   return (
-       <BrowserRouter basename="/my-portfolio">
+          <BrowserRouter basename="/my-portfolio">
+
       
   <Header/>
 
